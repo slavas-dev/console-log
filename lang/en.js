e8c7d0215a20eada8582f939880d33b3d937d4e7
slavas-dev <slavasfullstackdelveloper@gmail.com>
2025-08-27 20:50:34 +0300

    Create en.js

---
+++ lang/en.js
@@ -0,0 +1,13 @@
+module.exports = {
+    errors: {
+        channelNotFound: 'Channel with ID {channelId} not found',
+        notTextChannel: 'Channel is not a text channel',
+        discordClientError: 'Discord client error: {error}',
+        sendToDiscordFailed: 'Failed to send log to Discord: {error}',
+        languageNotFound: 'Language file for \'{language}\' not found, falling back to \'{defaultLang}\'',
+        defaultLanguageNotFound: 'Default language file not found'
+    },
+    logs: {
+        unknown: 'unknown'
+    }
+};
